34f6e0d9b57c07cbf145e83675bcbe989f58e933
tasneem vora <tasneemd18@gmail.com>
2019-10-02 22:25:44 -0400

    Code modified to work with database

--- 09-SQL_homework/SelectQueries.sql
+++ 09-SQL_homework/SelectQueries.sql
@@ -11,13 +11,15 @@ last_name AS "Last Name", first_name AS "First Name", gender, salary
 from employees 
 INNER JOIN salaries ON
 employees.emp_no = salaries.emp_no;
+
 --2)List employees who were hired in 1986.
-select count(*) from employees where extract(year from hire_date)='1986';
+select * from employees where extract(year from hire_date)='1986';
 
 --3)List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name, and start and end employment dates
-SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, 
-employees.first_name, employees.last_name , employees.emp_no,  dept_manager.from_date, dept_manager.to_date
+SELECT departments.dept_no AS "Department Number", departments.dept_name AS "Department Name", employees.emp_no AS "Employee Number", 
+employees.first_name AS "First Name", employees.last_name AS "Last Name", dept_manager.from_date AS "Emp Start Date", 
+dept_manager.to_date AS "Emp To Date"
 FROM employees 
 inner join dept_manager
 on employees.emp_no = dept_manager.emp_no
@@ -27,28 +29,30 @@ on departments.dept_no =dept_manager.dept_no;
 
 --4)List the department of each employee with the following information: 
 --employee number, last name, first name, and department name.
-SELECT departments.dept_name, employees.emp_no,  employees.last_name, employees.first_name
+SELECT employees.emp_no AS "Employee Number",  employees.last_name AS "Last Name", employees.first_name AS "First Name",
+departments.dept_name AS "Department Name"
 FROM employees 
 inner join dept_emp
 on employees.emp_no = dept_emp.emp_no
 inner join departments 
 on departments.dept_no =dept_emp.dept_no;
+
 --5)List all employees whose first name is "Hercules" and last names begin with "B."
-SELECT count(*) from employees WHERE first_name='Hercules' and last_name LIKE 'B%';
+SELECT * from employees WHERE first_name='Hercules' and last_name LIKE 'B%';
+
 --6)List all employees in the Sales department, including their employee number, last name, first name, and department name.
-select * from employees where emp_no IN (
-	SELECT emp_no from dept_emp WHERE dept_no IN(
-		SELECT dept_no from departments where dept_name='Sales'
-	)
-)
+SELECT employees.emp_no AS "Employee Number",  employees.last_name AS "Last Name", employees.first_name AS "First Name",
+departments.dept_name AS "Department Name"
+FROM employees 
+inner join dept_emp
+on employees.emp_no = dept_emp.emp_no
+inner join departments 
+on departments.dept_no =dept_emp.dept_no AND departments.dept_name ='Sales' ;
+
 --7)List all employees in the Sales and Development departments, including their employee number, 
 --last name, first name, and department name.
-select * from employees where emp_no IN (
-	SELECT emp_no from dept_emp WHERE dept_no IN(
-		SELECT dept_no from departments where dept_name IN('Sales','Development')
-	)
-)
-SELECT departments.dept_name, employees.emp_no,  employees.last_name, employees.first_name
+SELECT departments.dept_name AS "Department Name", 
+employees.emp_no AS "Employee Number",  employees.last_name AS "Last Name", employees.first_name AS "First Name"
 FROM employees 
 inner join dept_emp
 on employees.emp_no = dept_emp.emp_no
@@ -60,3 +64,15 @@ SELECT last_name, COUNT(last_name) AS "lastname count"
 FROM employees
 GROUP BY last_name
 ORDER BY "lastname count" DESC;
+
+
+--Search your ID number.
+SELECT departments.dept_name AS "Department Name", 
+employees.emp_no AS "Employee Number",  employees.last_name AS "Last Name", employees.first_name AS "First Name"
+FROM employees 
+inner join dept_emp
+on employees.emp_no = dept_emp.emp_no
+inner join departments 
+on departments.dept_no =dept_emp.dept_no AND employees.emp_no = '499942';
+
+
---
+++ 09-SQL_homework/databaseconfig.py
@@ -0,0 +1,5 @@
+database = "emp_dept_db"
+user = "postgres"
+host = "localhost"
+port = "5432"
+passw = "*****"
